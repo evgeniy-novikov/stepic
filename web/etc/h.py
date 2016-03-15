45bccdff5fc957a2156478dd2cd40e6095e6a88a
Novikov Evgeniy <evgeniy.v.novikov@gmail.com>
2016-03-15 09:03:42 -0400

    new

--- web/etc/h.py
+++ web/etc/h.py
@@ -1,11 +1,27 @@
+#CONFIG = {
+#    'mode': 'wsgi',
+#    'working_dir': '/home/box/web/'
+    # 'python': '/usr/bin/python',
+#    'args': (
+#	'--bind=0.0.0.0:8080',
+#	'--workers=16',
+#	'--timeout=60',
+#	'hello.app',
+#    ),
+#}
 CONFIG = {
     'mode': 'wsgi',
-    'working_dir': '/home/box/web/'
+    'working_dir': '/home/box/web/ask',
     # 'python': '/usr/bin/python',
     'args': (
-	'--bind=0.0.0.0:8080',
-	'--workers=16',
-	'--timeout=60',
-	'hello.app',
+	'--bind=0.0.0.0:8000',
+        '--access-logfile acc.log',
+        '--error-logfile err.log',
+        #'--daemon',
+        '--workers=4',
+        '--timeout=60',
+        #'wsgi',
+        'ask.wsgi:application',
+        #'hello:app',
     ),
 }
